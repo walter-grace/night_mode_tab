f6eb3b8c3cf52808e22971f7bdaa65e2a34b74b4
walter-grace <nico.zahniser@gmail.com>
2020-05-11 14:28:35 -0700

    code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import DarkModeToggle from './DarkModeToggle';
+import DraggableList from './components/MenuPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <DarkModeToggle />
+      <DraggableList
+      items={'Bitcoin Ethereum Tether Cardano'.split(' ')}
+      />
+      </div>
     </div>
   );
 }
